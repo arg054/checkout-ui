3cf79efe9d149daf78ae39ea09305e9d240c0ce9
arg054 <maxim.golovenkov@gmail.com>
2024-09-18 14:06:09 +0200

    lesson 2, formControl and input component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
-const IndexPage = () => <h1>Hello Next.js</h1>;
+import { Container, Flex, VStack } from "@chakra-ui/react";
+
+const IndexPage = () => (
+  <Container maxWidth="container.xl" padding={0}>
+    <Flex height="100vh" py={20}>
+      <VStack
+        width="full"
+        height="full"
+        padding={10}
+        spacing={10}
+        alignItems="flex-start"
+      ></VStack>
+      <VStack
+        width="full"
+        height="full"
+        padding={10}
+        spacing={10}
+        alignItems="flex-start"
+        background={"gray.50"}
+      ></VStack>
+    </Flex>
+  </Container>
+);
 
 export default IndexPage;
